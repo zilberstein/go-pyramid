42029cf4f493b70b552949673d3ea09aa2f25b2f
Noam Zilberstein <noamzilberstein@me.com>
2023-08-20 09:30:26 -0700

    io bool

--- AI/Naive.hs
+++ AI/Naive.hs
@@ -7,19 +7,22 @@ import Pyramid
 import System.Random
 
 strategy :: Monad m => Strategy m 
-strategy = keepIf $ const False
+strategy = keepIf $ const $ return False
 
 hoard :: Monad m => Label -> Strategy m 
-hoard lbl = keepIf $ \(Card l _) -> l == lbl
+hoard lbl = keepIf $ \(Card l _) -> return $ l == lbl
 
-keepIf :: Monad m => (Card -> Bool) -> Strategy m
-keepIf f Player{..} _ _ = return $ Do $ reverse (getActions pHand (pPyramid, []))
+keepIf :: Monad m => (Card -> m Bool) -> Strategy m
+keepIf f Player{..} _ _ = Do <$> getActions pHand (pPyramid, [])
   where
-    getActions (card:cards) (pPyramid, list) = getActions cards (tryInsert card pPyramid list)
-    getActions [] (pPyramid, list) = list
+    getActions (card:cards) (pPyramid, list) = getActions cards =<< tryInsert card pPyramid list
+    getActions [] (pPyramid, list) = return $ reverse list
     tryInsert card pPyramid actions =
       case add card pPyramid of
-        Just pyramid -> (pyramid, Put:actions)
-        Nothing
-          | f card    -> (pPyramid, Keep:actions)
-          | otherwise -> (pPyramid, Throw:actions)
+        Just pyramid -> return (pyramid, Put:actions)
+        Nothing -> do
+          keep <- f card
+          if keep then
+            return (pPyramid, Keep:actions)
+            else
+            return (pPyramid, Throw:actions)
